fd67d977d2d89bbddfd5aceebc89c87955972257
Martin Sotelo <lmsotelo@fi.uba.ar>
2023-11-25 21:39:37 -0300

    correcion de check tarea completada

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+/*import React, { useState, useEffect, useRef } from 'react';
 import Swal from 'sweetalert2';
 
 const TaskItem = ({ task, handleComplete, handleDelete, handleToggleMenu }) => {
@@ -50,13 +50,13 @@ const TaskItem = ({ task, handleComplete, handleDelete, handleToggleMenu }) => {
       <span className={task.completed ? 'task-name completed' : 'task-name'}>
         {task.name}
       </span>
-      {/* Mostrar la categoría en el menú desplegable */}
+      {// Mostrar la categoría en el menú desplegable }
       <div className="menu-button" onClick={toggleMenu}>
         &#8942;
         {showMenu && (
           <div className="task-menu" ref={menuRef}>
             <div className="menu-item">
-              {/* Verificar si la tarea tiene una categoría y mostrarla */}
+              {// Verificar si la tarea tiene una categoría y mostrarla }
               {task.category ? `Categoría: ${task.category}` : 'Sin categoría'}
             </div>
             <div
@@ -76,7 +76,91 @@ const TaskItem = ({ task, handleComplete, handleDelete, handleToggleMenu }) => {
             >
               Eliminar tarea
             </div>
-            {/* Agregar aquí otra funcionalidad adicional */}
+            {}
+          </div>
+        )}
+      </div>
+    </li>
+  );
+};
+
+export default TaskItem;*/
+import React, { useState, useEffect, useRef } from 'react';
+import Swal from 'sweetalert2';
+
+const TaskItem = ({ task, handleComplete, handleDelete, handleToggleMenu }) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef();
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleCompleteWithAlert = () => {
+    let completed = !task.completed;
+
+    handleComplete(task.id, completed);
+    setShowMenu(false);
+
+    if (completed) {
+      Swal.fire({
+        icon: 'success',
+        title: '¡Tarea completada!',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    if (showMenu) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showMenu]);
+
+  return (
+    <li className="task-item">
+      <div
+        className="task-status"
+        onClick={handleCompleteWithAlert}
+      >
+        {task.completed && <span className="checkmark">&#10003;</span>}
+      </div>
+      <span className={task.completed ? 'task-name completed' : 'task-name'}>
+        {task.name}
+      </span>
+      <div className="menu-button" onClick={toggleMenu}>
+        &#8942;
+        {showMenu && (
+          <div className="task-menu" ref={menuRef}>
+            <div className="menu-item">
+              {task.category ? `Categoría: ${task.category}` : 'Sin categoría'}
+            </div>
+            <div
+              className="menu-item"
+              onClick={handleCompleteWithAlert}
+            >
+              {task.completed ? 'Desmarcar tarea' : 'Marcar como hecha'}
+            </div>
+            <div
+              className="menu-item"
+              onClick={() => {
+                handleDelete(task.id);
+                setShowMenu(false);
+              }}
+            >
+              Eliminar tarea
+            </div>
           </div>
         )}
       </div>
@@ -85,6 +169,7 @@ const TaskItem = ({ task, handleComplete, handleDelete, handleToggleMenu }) => {
 };
 
 export default TaskItem;
+
 
 
 
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,86 +1,3 @@
-/*import React, { useState, useEffect } from "react";
-import DateNavigator from "./DateNavigator";
-import TaskForm from "./TaskForm";
-import TaskItem from "./TaskItem";
-
-const TaskList = () => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [isFormVisible, setFormVisible] = useState(false);
-  const [tasks, setTasks] = useState(() => {
-    const savedTasks = localStorage.getItem("tasks");
-    return savedTasks ? JSON.parse(savedTasks) : [];
-  });
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  const addTask = (task) => {
-    setTasks([...tasks, { ...task, date: selectedDate, completed: false }]);
-    setFormVisible(false);
-  };
-
-  const handleComplete = (taskId) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === taskId ? { ...task, completed: !task.completed } : task
-      )
-    );
-  };
-
-  const handleDelete = (taskId) => {
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  };
-
-  const handleToggleMenu = (taskId) => {
-    const updatedTasks = tasks.map((task) =>
-      task.id === taskId ? { ...task, showMenu: !task.showMenu } : task
-    );
-    setTasks(updatedTasks);
-  };
-
-  const filteredTasks = tasks.filter(
-    (task) =>
-      task.date &&
-      new Date(task.date).toDateString() === selectedDate.toDateString()
-  );
-
-  return (
-    <>
-      <div className="form-border">
-        <div className="form-general">
-          <DateNavigator
-            selectedDate={selectedDate}
-            onDateChange={setSelectedDate}
-            onAddClick={() => setFormVisible(true)}
-          />
-          {isFormVisible && (
-            <TaskForm
-              addTask={addTask}
-              isVisible={isFormVisible}
-              onClose={() => setFormVisible(false)}
-            />
-          )}
-          <ul>
-            {filteredTasks.map((task) => (
-              <TaskItem
-                key={task.id}
-                task={task}
-                handleComplete={() => handleComplete(task.id)}
-                handleDelete={() => handleDelete(task.id)}
-                handleToggleMenu={() => handleToggleMenu(task.id)}
-              />
-            ))}
-          </ul>
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default TaskList;*/
-// tasklist.jsx
-
 import React, { useState, useEffect } from "react";
 import DateNavigator from "./DateNavigator";
 import TaskForm from "./TaskForm";
@@ -162,3 +79,5 @@ const TaskList = () => {
 };
 
 export default TaskList;
+
+
